e6510437ead4a16dc336a6d1c251f8fbc61efef0
kosskas <93159483+kosskas@users.noreply.github.com>
2025-03-16 22:13:45 +0100

    Add basic scraper

---
+++ Graph.py
@@ -0,0 +1,48 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.drawing import nx_pydot
+import re
+import random
+
+
+class WWWGraph:
+    def __init__(self, pages_limit):
+        self.G = nx.DiGraph()
+        self.pages_limit = pages_limit
+
+    def addDirectedEdge(self, page, sub_page):
+        self.G.add_edge(page, sub_page)
+
+    def basic_stats(self):
+        """Zwraca podstawowe statystyki grafu"""
+        num_nodes = len(self.G.nodes())
+        num_edges = len(self.G.edges())
+        return num_nodes, num_edges
+
+    def printGraph(self):
+
+        valid_names = {
+            node: re.sub(r"https?://", "", node).replace("/", "-").replace(".", "-")
+            for node in self.G.nodes
+        }
+        self.G = nx.relabel_nodes(self.G, valid_names)
+        # pos = nx.spring_layout(self.G, scale=10.0)
+        # pos = nx.spring_layout(self.G, k=2.5, scale=100)
+        pos = nx.random_layout(self.G, seed=random.randint(0, 0xFFFFFFFF))
+
+        for node, _ in pos.items():
+            self.G.nodes[node][
+                "node_options"
+            ] = "circle, draw=black, fill=blue,minimum size=2pt"
+            self.G.nodes[node]["node_label"] = ""
+        # to_latex_raw only tkiz
+        latex = nx.to_latex(
+            self.G,
+            pos=pos,
+            node_label="node_label",
+            node_options="node_options",
+        )
+
+        latex = latex.replace("\\begin{scope}[->]", "\\begin{scope}[->, opacity=0.3]")
+        with open("graph.tex", "w") as f:
+            f.write(latex)
---
+++ Robot.py
@@ -0,0 +1,80 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import threading
+from queue import Queue
+from urllib.robotparser import RobotFileParser
+from urllib.parse import urljoin, urlparse
+from Graph import WWWGraph
+import re
+
+DIR = "./downloaded_pages"
+
+
+class WWWRobot:
+    def __init__(self, start_url, pages_limit):
+        self.start_url = start_url
+        self.visited_pages = set()
+        self.pages_limit = pages_limit
+        self.queue = Queue()
+        self.www_graph = WWWGraph(pages_limit)
+        self.robot_parser = RobotFileParser()
+        self.robot_parser.set_url(urljoin(start_url, "robots.txt"))
+        self.robot_parser.read()
+
+    def is_allowed(self, url):
+        return self.robot_parser.can_fetch("*", url)
+
+    def get_page(self, url, idx):
+        if url not in self.visited_pages and self.is_allowed(url):
+            try:
+                print(f"getting {url}")
+                response = requests.get(url)
+                if response.status_code == 200:
+                    page_content = response.text
+                    self.visited_pages.add(url)
+                    return page_content
+            except requests.RequestException as e:
+                print(f"Error reading page {url}: {e}")
+        return None
+
+    def validate_url(self, url):
+        url = re.sub(r"#.*", "", url)  # delete subsection
+        url = re.sub(r"\?.*", "", url)  # delete get method
+
+        if url[-1] == "/":
+            url = url[:-1]
+        return url
+
+    def parse_links(self, page_content, url):
+        dom = BeautifulSoup(page_content, "html.parser")
+        links = set()
+        for anchor in dom.find_all("a", href=True):
+            link = urljoin(url, anchor["href"])
+            if urlparse(link).netloc == urlparse(self.start_url).netloc:
+                if (
+                    not re.search(r"\.(pdf|docx)$", link)
+                    and not re.match(r"mailto:", link)
+                    and not re.match(r"tel:", link)
+                ):
+                    links.add(self.validate_url(link))
+        return links
+
+    def start(self):
+        if not os.path.exists(DIR):
+            os.makedirs(DIR)
+        idx = 0
+        self.queue.put(self.start_url)
+        while not self.queue.empty():
+            print(f"Queue {self.queue.qsize()}")
+            url = self.queue.get()
+            page = self.get_page(url, idx)
+            if page is not None:
+                links = self.parse_links(page, url)
+                for link in links:
+                    if link not in self.visited_pages and idx < self.pages_limit:
+                        self.queue.put(link)
+                        self.www_graph.addDirectedEdge(url, link)
+                        idx += 1
+        self.www_graph.printGraph()
+        print(self.visited_pages)
