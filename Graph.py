b7dea1d03ea3778937f3816a051dc4778a9621fc
kosskas <93159483+kosskas@users.noreply.github.com>
2025-04-17 22:57:18 +0200

    add atacks

--- Graph.py
+++ Graph.py
@@ -1,3 +1,4 @@
+import copy
 import networkx as nx
 import matplotlib
 
@@ -18,6 +19,7 @@ class WWWGraph:
         self.G = nx.DiGraph()
         self.pages_limit = pages_limit
         self.pages_num = {}
+        self.label = ""
 
     def addDirectedEdge(self, page, sub_page):
         self.G.add_edge(self.format_link(page), self.format_link(sub_page))
@@ -36,29 +38,29 @@ class WWWGraph:
     def save(self):
         nx.write_gml(self.G, "graph.gml")
 
-    def printGraph(self):
+    def printGraph(self, graph):
 
         valid_names = {
             node: re.sub(r"https?://", "", node)
             .replace("/", "-")
             .replace(".", "-")
             .replace("%", "-")
-            for node in self.G.nodes
+            for node in graph.nodes
         }
-        self.G = nx.relabel_nodes(self.G, valid_names)
-        pos = nx.spring_layout(self.G, k=2.5)
-        # pos = nx.spring_layout(self.G, k=0.5)
-        # pos = nx.spring_layout(self.G, k=2)
-        # pos = nx.random_layout(self.G, seed=random.randint(0, 0xFFFFFFFF))
+        graph = nx.relabel_nodes(graph, valid_names)
+        pos = nx.spring_layout(graph, k=2.5)
+        # pos = nx.spring_layout(graph, k=0.5)
+        # pos = nx.spring_layout(graph, k=2)
+        # pos = nx.random_layout(graph, seed=random.randint(0, 0xFFFFFFFF))
 
         for node, _ in pos.items():
-            self.G.nodes[node][
+            graph.nodes[node][
                 "node_options"
             ] = "circle, draw=black, fill=blue,minimum size=2pt"
-            self.G.nodes[node]["node_label"] = ""
+            graph.nodes[node]["node_label"] = ""
         # to_latex_raw only tkiz
         latex = nx.to_latex_raw(
-            self.G,
+            graph,
             pos=pos,
             node_label="node_label",
             node_options="node_options",
@@ -69,7 +71,7 @@ class WWWGraph:
         with open("graph.tex", "w", encoding="utf-8") as f:
             f.write(latex)
 
-    def extract_subgraph(graph, node):
+    def extract_subgraph(self, graph, node):
         # N and N
         neighbors = set(graph.neighbors(node))
         second_neighbors = set()
@@ -86,23 +88,30 @@ class WWWGraph:
         with open(f"{filename}.txt", "w", encoding="utf-8") as f:
             f.write(text)
 
-    def basic(self):
-        print(f"V={self.G.number_of_nodes()}, E={self.G.number_of_edges()}")
+    def basic(self, graph):
+        print(
+            f"[{self.label}] V={graph.number_of_nodes()}, E={graph.number_of_edges()}"
+        )
 
-    def InOutDeg(self):
+    def InOutDeg(self, graph):
         # rozkłady stopni (in, out), wyznaczenie współczynników funkcji potęgowej metodami analitycznymi (np. regresja),
-        in_deg = Counter(Counter(dict(self.G.in_degree())).values())
-        out_deg = Counter(Counter(dict(self.G.out_degree())).values())
+        in_deg = Counter(Counter(dict(graph.in_degree())).values())
+        out_deg = Counter(Counter(dict(graph.out_degree())).values())
         x_in, y_in = zip(*in_deg.items())
         x_out, y_out = zip(*out_deg.items())
 
         inSort = sorted([i for i in in_deg.keys()])
-        self.save_file("in_deg", "".join([f"({i}, {in_deg[i]})\n" for i in inSort]))
+        self.save_file(
+            f"in_deg_{self.label}", "".join([f"({i}, {in_deg[i]})\n" for i in inSort])
+        )
         outSort = sorted([i for i in out_deg.keys()])
-        self.save_file("out_deg", "".join([f"({i}, {out_deg[i]})\n" for i in outSort]))
+        self.save_file(
+            f"out_deg_{self.label}",
+            "".join([f"({i}, {out_deg[i]})\n" for i in outSort]),
+        )
 
-        self.print_linregress(x_in, y_in, "Wejściowe")
-        self.print_linregress(x_out, y_out, "Wyjściowe")
+        self.print_linregress(x_in, y_in, f"[{self.label}] Wejściowe")
+        self.print_linregress(x_out, y_out, f"[{self.label}] Wyjściowe")
 
     def print_linregress(self, x, y, label):
         non_zero_x = np.array(x)[np.array(x) > 0]
@@ -114,22 +123,22 @@ class WWWGraph:
         slope, intercept, r_value, p_value, std_err = linregress(log_x, log_y)
         b = slope
         a = np.exp(intercept)
-        print(f"Wzór rozkładu potęgowego ({label}):")
-        print(f"P(k) = {a:.4f} * x^{b:.4f}")
-        print(f"R^2 = {r_value**2:.2f}\n")
+        print(f"[{self.label}]  Wzór rozkładu potęgowego ({label}):")
+        print(f"[{self.label}] P(k) = {a:.4f} * x^{b:.4f}")
+        print(f"[{self.label}] R^2 = {r_value**2:.2f}\n")
 
-    def SCCWCC(self):
+    def SCCWCC(self, graph):
         # analiza składowych spójności: słabe (WCC), silne (SCC), komponenty IN, OUT, graf SCC (podział na SCC),
         wcc = len(
-            list(nx.connected_components(self.G.to_undirected()))
-        )  # len(list(nx.weakly_connected_components(self.G)))
-        scc = len(list(nx.strongly_connected_components(self.G)))
+            list(nx.connected_components(graph.to_undirected()))
+        )  # len(list(nx.weakly_connected_components(graph)))
+        scc = len(list(nx.strongly_connected_components(graph)))
 
-        print(f"Liczba słabych komponentów spójności (WCC): {wcc}")
-        print(f"Liczba silnych komponentów spójności (SCC): {scc}")
+        print(f"[{self.label}]  Liczba słabych komponentów spójności (WCC): {wcc}")
+        print(f"[{self.label}]  Liczba silnych komponentów spójności (SCC): {scc}")
 
-    def distances(self):
-        shortest_paths = dict(nx.all_pairs_shortest_path_length(self.G))
+    def distances(self, graph):
+        shortest_paths = dict(nx.all_pairs_shortest_path_length(graph))
 
         # średnie odległości dla każdej pary
         distances = []
@@ -140,10 +149,12 @@ class WWWGraph:
 
         avg_distance = np.mean(distances)
         counter = Counter(distances)
-        print(f"distances {counter}")
-        print(f"Średnia odległość w grafie: {avg_distance}")
+        print(f"[{self.label}]  distances {counter}")
+        print(f"[{self.label}]  Średnia odległość w grafie: {avg_distance}")
 
-        self.print_linregress(list(counter.keys()), list(counter.values()), "distances")
+        self.print_linregress(
+            list(counter.keys()), list(counter.values()), f"distances_{self.label}"
+        )
 
         x = np.array(list(counter.keys()))
         y = np.array(list(counter.values()))
@@ -151,40 +162,41 @@ class WWWGraph:
         # Regresja kwadratowa: y = a*x^2 + b*x + c
         coeffs = np.polyfit(x, y, 2)
         a, b, c = coeffs
-        print(f"Regresja kwadratowa: {a:.4f} * x2 + {b:.4f} * x + {c:.4f}")
+        print(
+            f"[{self.label}]  Regresja kwadratowa: {a:.4f} * x2 + {b:.4f} * x + {c:.4f}"
+        )
 
-    def diameter(self):
+    def diameter(self, graph):
         # Średnica grafu (maksymalna odległość)
-        diameter = nx.diameter(self.G)
-        print(f"Średnica grafu: {diameter}")
+        diameter = nx.diameter(graph)
+        print(f"[{self.label}]  Średnica grafu: {diameter}")
 
-    def radius(self):
+    def radius(self, graph):
         # Promień grafu (minimalna odległość do najdalszego wierzchołka)
-        radius = nx.radius(self.G)
-        print(f"Promień grafu: {radius}")
+        radius = nx.radius(graph)
+        print(f"[{self.label}] Promień grafu: {radius}")
 
-    def cluster(self):
+    def cluster(self, graph):
         # współczynniki klasteryzacji: lokalne oraz globalne (analiza histogramów i regresja dla rozkładów)
         # Lokalny współczynnik klasteryzacji
-        local_clustering = nx.clustering(self.G)
+        local_clustering = nx.clustering(graph)
 
         # Globalny współczynnik klasteryzacji
-        # nx.average_clustering(self.G) wychodzi to samo co local_clustering
-        global_clustering = nx.transitivity(self.G.to_undirected())
+        # nx.average_clustering(graph) wychodzi to samo co local_clustering
+        global_clustering = nx.transitivity(graph.to_undirected())
 
         print(
-            f"Lokalny współczynnik klasteryzacji: {np.mean(list(local_clustering.values()))}"
+            f"[{self.label}] Lokalny współczynnik klasteryzacji: {np.mean(list(local_clustering.values()))}"
         )
-        print(f"Globalny współczynnik klasteryzacji: {global_clustering}")
+        print(
+            f"[{self.label}] Globalny współczynnik klasteryzacji: {global_clustering}"
+        )
         counter = Counter(round(v, 2) for v in local_clustering.values())
         self.print_linregress(
-            list(counter.keys()), list(counter.values()), "localclust2"
-        )
-        self.print_linregress(
-            list(counter.keys()), list(counter.values()), "localclust1"
+            list(counter.keys()), list(counter.values()), f"[{self.label}] localclust2"
         )
         self.save_file(
-            "localcluster2",
+            f"localcluster2_{self.label}",
             "".join(
                 f"({k}, {v})\n"
                 for k, v in Counter(
@@ -192,36 +204,34 @@ class WWWGraph:
                 ).items()
             ),
         )
-        self.save_file(
-            "localcluster1",
-            "".join(
-                f"({k}, {v})\n"
-                for k, v in Counter(
-                    round(v, 1) for v in local_clustering.values()
-                ).items()
-            ),
-        )
 
-    def vertex_connectivity(self):
+    def vertex_connectivityNP(self, graph):
         try:
             print("[INFO] Start node_connectivity obliczeń...")
-            undirected_G = self.G.to_undirected()
-            k = 1  # nx.node_connectivity(self.G)
+            undirected_G = graph.to_undirected()
+            k = 1  # nx.node_connectivity(graph)
             print(f"[OK] Vertex connectivity (node_connectivity) = {k}")
             cuts = 0
             if k == 1:
                 cuts = sum(1 for _ in nx.all_node_cuts(undirected_G))
-                print(f"k=1, cuts {cuts}")
+                print(f"[{self.label}] k=1, cuts {cuts}")
             elif k == 2:
-                pairs = len(list(nx.k_edge_components(self.G, k)))
-                print(f"k=2, cuts {pairs}")
+                pairs = len(list(nx.k_edge_components(graph, k)))
+                print(f"[{self.label}] k=2, cuts {pairs}")
             else:
                 print(k)
         except Exception as e:
             print(e)
 
-    def pagerank1(self):
-        pagerank = nx.pagerank(self.G, alpha=0.85)
+    def vertex_connectivity(self, graph):
+        print("[INFO] Start node_connectivity obliczeń...")
+        # k = nx.node_connectivity(graph)
+        pairs = list(nx.all_node_cuts(self.f, 2))
+        print(f"slabe {pairs}")
+        # print(f"[OK] Vertex connectivity (node_connectivity) = {k}")
+
+    def pagerank1(self, graph):
+        pagerank = nx.pagerank(graph, alpha=0.85)
         # Rozkład PageRank
         sorted_pagerank = dict(
             sorted(pagerank.items(), key=lambda item: item[1], reverse=True)
@@ -239,38 +249,19 @@ class WWWGraph:
         plt.title("Rozkład PageRank")
         plt.savefig("pagerank1.png")
 
-    def pagerank1(self):
-        pagerank = nx.pagerank(self.G, alpha=0.85)
-        # Rozkład PageRank
-        sorted_pagerank = dict(
-            sorted(pagerank.items(), key=lambda item: item[1], reverse=True)
-        )
-        vertex_to_id = {name: i for i, name in enumerate(sorted_pagerank.keys())}
-
-        x_vals = list(vertex_to_id.values())
-        y_vals = list(sorted_pagerank.values())
-
-        # Wykres
-        plt.figure(figsize=(10, 6))
-        plt.bar(x_vals, y_vals)
-        plt.xlabel("ID strony (kolejność według PageRank)")
-        plt.ylabel("Wartość PageRank")
-        plt.title("Rozkład PageRank")
-        plt.savefig("pagerank.png")
-
-    def custom_pagerank(self, alpha):
+    def custom_pagerank(self, graph, alpha):
         tol = 1e-6
         max_iter = 100
-        N = self.G.number_of_nodes()
-        nodes = list(self.G.nodes())
+        N = graph.number_of_nodes()
+        nodes = list(graph.nodes())
         ranks = {node: 1 / N for node in nodes}
         for _ in range(max_iter):
             new_ranks = {}
             for node in nodes:
                 rank_sum = sum(
-                    ranks[neigh] / self.G.out_degree(neigh)
-                    for neigh in self.G.predecessors(node)
-                    if self.G.out_degree(neigh) > 0
+                    ranks[neigh] / graph.out_degree(neigh)
+                    for neigh in graph.predecessors(node)
+                    if graph.out_degree(neigh) > 0
                 )
                 new_ranks[node] = (1 - alpha) / N + alpha * rank_sum
             delta = sum(abs(new_ranks[n] - ranks[n]) for n in nodes)
@@ -284,35 +275,86 @@ class WWWGraph:
 
         x_vals = list(vertex_to_id.values())
         y_vals = list(sorted_pagerank.values())
-        self.print_linregress(x_vals, y_vals, f"pg a={alpha}")
+        self.print_linregress(x_vals, y_vals, f"[{self.label}] pg a={alpha}")
         self.save_file(
-            f"pg a={alpha}",
+            f"pg a={alpha}_{self.label}",
             "".join(f"({k}, {v})\n" for k, v in zip(x_vals, y_vals)),
         )
 
-    def run_custom_pagerank(self):
+    def run_custom_pagerank(self, graph):
         print("\n[INFO] Analiza zbieżności przy różnych wartościach tłumienia:")
         for d in [1, 0.85, 0.7, 0.55]:
             print(f"\nalfa = {d}")
             self.custom_pagerank(alpha=d)
 
+    def simulate_failure_and_attack_scenarios(self, Graph, idx, seed=None):
+
+        def remove_nodes(g, nodes_to_remove):
+            g_copy = copy.deepcopy(g)
+            g_copy.remove_nodes_from(nodes_to_remove)
+            return g_copy
+
+        random.seed(seed)
+        n = Graph.number_of_nodes()
+        nodes = list(Graph.nodes)
+
+        degree_sorted = sorted(Graph.degree(), key=lambda x: x[1], reverse=True)
+        attack_10 = [node for node, _ in degree_sorted[: int(0.10 * n)]]
+        attack_20 = [node for node, _ in degree_sorted[: int(0.20 * n)]]
+        attack_30 = [node for node, _ in degree_sorted[: int(0.30 * n)]]
+        attack_50 = [node for node, _ in degree_sorted[: int(0.50 * n)]]
+        failure_10 = random.sample(nodes, int(0.10 * n))
+        failure_20 = random.sample(nodes, int(0.20 * n))
+        failure_30 = random.sample(nodes, int(0.30 * n))
+        failure_50 = random.sample(nodes, int(0.50 * n))
+        scenarios = None
+
+        if 1 == idx:
+            scenarios = remove_nodes(Graph, [nodes[0]])
+        if 2 == idx:
+            scenarios = remove_nodes(Graph, failure_10)
+        if 3 == idx:
+            scenarios = remove_nodes(Graph, failure_20)
+        if 4 == idx:
+            scenarios = remove_nodes(Graph, failure_30)
+        if 5 == idx:
+            scenarios = remove_nodes(Graph, failure_50)
+        if 6 == idx:
+            scenarios = remove_nodes(Graph, attack_10)
+        if 7 == idx:
+            scenarios = remove_nodes(Graph, attack_20)
+        if 8 == idx:
+            scenarios = remove_nodes(Graph, attack_30)
+        if 8 == idx:
+            scenarios = remove_nodes(Graph, attack_50)
+
+        return scenarios
+
     def analyze_graph(self):
         workers = 4
         functions = [
-            # self.basic,
-            # self.SCCWCC,
-            # self.InOutDeg,
-            # self.distances,
+            self.basic,
+            self.SCCWCC,
+            self.InOutDeg,
+            self.distances,
             # self.cluster,
             # self.diameter,
             # self.radius,
-            self.run_custom_pagerank,
+            # self.run_custom_pagerank,
+            self.vertex_connectivity,
         ]
-
-        with ThreadPoolExecutor(max_workers=workers) as executor:
-            futures = [executor.submit(func) for func in functions]
-            for future in futures:
-                future.result()
+        for idx in range(1, 8):
+            with ThreadPoolExecutor(max_workers=workers) as executor:
+                print(f"===============tryb {idx}=====================")
+                self.label = f"tryb_{idx}"
+                graph = self.simulate_failure_and_attack_scenarios(self.G, idx, 23)
+                futures = [executor.submit(func, graph) for func in functions]
+                for future in futures:
+                    future.result()
+            with ThreadPoolExecutor(max_workers=workers) as executor:
+                futures = [executor.submit(func, self.G) for func in functions]
+                for future in futures:
+                    future.result()
 
 
 if __name__ == "__main__":
